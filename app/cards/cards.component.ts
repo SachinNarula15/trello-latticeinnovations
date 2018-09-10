ba5e0cee6b65dc70eed4e35d223f3c0623461bf5
SachinNarula15 <36657726+SachinNarula15@users.noreply.github.com>
2018-09-11 03:32:30 +0530

    Shortcut keys enabled in all pages

--- app/cards/cards.component.ts
+++ app/cards/cards.component.ts
@@ -3,9 +3,64 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
+  host: {'(window:keydown)': 'hotkeys($event)'},
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
+
+  hotkeys(event){
+    if(event.keyCode == 88 && event.ctrlKey){
+    this.newElement();
+  },
+  if(event.keyCode == 89 && event.ctrlKey){
+    this.newElement1();
+  },
+  if(event.keyCode == 90 && event.ctrlKey){
+    this.newElement2();
+  }
+}
+
+// Script for first card 
+  newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+    alert("You must write something!");
+    } else {
+    document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+    }
+
+    // Script for second card
+    newElement1() {
+      var li = document.createElement("li");
+      var inputValue = document.getElementById("myInput1").value;
+      var t = document.createTextNode(inputValue);
+      li.appendChild(t);
+      if (inputValue === '') {
+      alert("You must write something!");
+      } else {
+      document.getElementById("myUL1").appendChild(li);
+      }
+      document.getElementById("myInput1").value = "";
+      }
+
+     // Script for second card
+     newElement2() {
+      var li = document.createElement("li");
+      var inputValue = document.getElementById("myInput2").value;
+      var t = document.createTextNode(inputValue);
+      li.appendChild(t);
+      if (inputValue === '') {
+      alert("You must write something!");
+      } else {
+      document.getElementById("myUL2").appendChild(li);
+      }
+      document.getElementById("myInput2").value = "";
+      }
 
   constructor() { }
 
--- app/header/header.component.ts
+++ app/header/header.component.ts
@@ -3,9 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  host: {'(window:keydown)': 'hotkeys($event)'},
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+  hotkeys(event){
+    if(event.keyCode == 66){
+      $(".board-section").toggleClass("open");
+  },
+  if(event.keyCode == 191){
+    $(".searchbar").toggleClass("style");
+  }
+}
 
   constructor() { }
 
